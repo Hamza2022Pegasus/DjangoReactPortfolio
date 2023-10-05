511c10bec5d8bb551f2c19b5e62daf139c302a98
Hamza2022Pegasus <power.eater1@gmail.com>
2023-10-05 01:32:30 -0700

    updated meta names for urls

--- portfolioFolder/portfolio/appForProject/urls.py
+++ portfolioFolder/portfolio/appForProject/urls.py
@@ -5,11 +5,11 @@ from . import views
 
 urlpatterns = [
     path('intro-details', views.GetIntroDetails.as_view(), name="IntroBanner"),
-    path('about-details', views.GetAboutDetails.as_view(), name="IntroBanner"),
-    path('services-details', views.GetServicesDetails.as_view(), name="IntroBanner"),
-    path('technologies-details', views.GetTechnologiesDetails.as_view(), name="IntroBanner"),
-    path('jobs-details', views.GetJobsDetails.as_view(), name="IntroBanner"),
-    path('contacts-details', views.GetContactDetails.as_view(), name="IntroBanner"),
-    path('social-details', views.GetSMLinksDetails.as_view(), name="IntroBanner"),
+    path('about-details', views.GetAboutDetails.as_view(), name="AboutDetails"),
+    path('services-details', views.GetServicesDetails.as_view(), name="ServicesDetails"),
+    path('technologies-details', views.GetTechnologiesDetails.as_view(), name="TechnologiesDetails"),
+    path('jobs-details', views.GetJobsDetails.as_view(), name="JobsDetails"),
+    path('contacts-details', views.GetContactDetails.as_view(), name="ContactsDetails"),
+    path('social-details', views.GetSMLinksDetails.as_view(), name="SocialDetails"),
     path('send_email',views.proposal, name="sendEmail")
 ]
